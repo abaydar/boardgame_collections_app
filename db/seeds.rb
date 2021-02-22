277a00a4c6e8fa47ff8c535ce2b97102c91210dc
+ <+>
2021-02-22 13:51:55 -0500

    create users using faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #     Boardgame.create(name: bg["name"], year_published: bg["yearPublished"], image: bg["image"], min_players: bg["minPlayers"], max_players: bg["maxPlayers"], playing_time: bg["playingTime"])
 # end
 
+# 50.times do 
+#     name = Faker::Name.name
+#     username = (Faker::Name.initials(number: 1) + Faker::Name.last_name).downcase
+#     email = Faker::Internet.free_email
+#     password = Faker::Internet.password
+
+#     user_hash = {name: name, username: username, email: email, password: password}
+
+#     User.create(user_hash)
+    
+# end
