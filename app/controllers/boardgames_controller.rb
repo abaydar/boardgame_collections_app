452de6ec6c3e3c1c5acffb4a4e6f5c1e78ef1d9c
+ <+>
2021-02-24 14:21:01 -0500

    view edit/delete button for creators

--- app/controllers/boardgames_controller.rb
+++ app/controllers/boardgames_controller.rb
@@ -14,27 +14,58 @@ class BoardgamesController < ApplicationController
     end
 
     post '/boardgames' do 
-        bg = Boardgame.new(params[:boardgame])
-        user = User.find(session[:user_id])
-        bg.creator_id = user.id
-        user.boardgames << bg 
-        bg.save
-        redirect "/boardgames/#{bg.id}"
+        if logged_in?
+            bg = Boardgame.new(params[:boardgame])
+            bg.creator_id = current_user.id
+            current_user.boardgames << bg 
+            bg.save
+            redirect "/boardgames/#{bg.id}"
+        else 
+            redirect '/login'
+        end
     end
 
     get '/boardgames/:id' do
-        @boardgame = Boardgame.find(params[:id])
+        get_boardgame
 
         erb :'boardgames/show'
     end
 
     get '/boardgames/:id/edit' do 
-        if !logged_in?
-            redirect '/login'
-        else
-            erb :'boardgames/edit'
+       get_boardgame
+       redirect_if_not_authorized
+       erb :'/boardgames/edit'
+    end
+
+    patch '/boardgames/:id' do 
+        get_boardgame
+        redirect_if_not_authorized
+        @boardgame.update(params[:boardgame])
+        redirect "/boardgames/#{@boardgame.id}"    
+    end
+
+    delete '/boardgames/:id' do
+        get_boardgame
+        redirect_if_not_authorized
+        @boardgame.delete
+        redirect '/boardgames'
+    end
+
+
+
+private 
+
+    def get_boardgame
+        @boardgame = Boardgame.find_by(id: params[:id])
+    end
+
+    def redirect_if_not_authorized
+        if @boardgame.creator_id != current_user.id 
+            flash[:message] = "You can't edit a post you haven't created"
+            redirect '/boardgames'
         end
     end
+
 
 
 end
